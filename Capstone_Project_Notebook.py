eeb8a4f936d10056417492e415146388757b5aa7
Zola Sibanda <Zola@moiponegroup.co.za>
2020-02-14 15:05:05 +0200

    adds the Capstone Project Notebook

---
+++ Capstone_Project_Notebook.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Peer-graded Assignment - Capstone Project Notebook
+
+# This Jupyter notebook will be mainly used for the Capstone Project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
